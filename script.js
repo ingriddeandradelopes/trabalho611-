064e3550c1266f089f798e715dec9a20601fe374
ingriddeandradelopes <163137409+ingriddeandradelopes@users.noreply.github.com>
2024-11-06 08:30:52 -0300

    .

--- script.js
+++ script.js
@@ -4,28 +4,28 @@ function alterartexto() {
 }
 
 // adicionar um novo paragrafo ao final do corpo 
-function adicionarparacrafo() {
-    const novoparacrafo = documento.createelement("p");
-    novoparacrafo.innertext = "depois que voce clicou ,eu apareci!!";
-    documento.body.appendchild(novoparacrafo);
+function adicionarparagrafo() {
+    const novoparagrafo = documento.createElement("p");
+    novoparagrafo.innertext = "depois que voce clicou ,eu apareci!!";
+    documento.body.appendChild(novoparagrafo);
 }
 
 //remover o ultimo paracrafo, se houver mais de um
-function removerparacrafo() {
+function removerparagrafo() {
    const paragrafos = document.querySelectorAll("p");
    if (paragrafos.length > 1) {
-    paragrafos[paragrafos.length - 1].remover();
+    paragrafos[paragrafos.length - 1].remove();
    }
 }
 
 //mudar a cor do texto do pagina  para uma cor aleatoria
 function mudarcorfundo() {
-    const coraleatoria = `#${math.floor(math.random() * 16777215).toString(16)}`;
-    document.body.style.backgroundColor = coraleatoria; 
+    const corAleatoria = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    document.body.style.backgroundColor = corAleatoria; 
 }
 
 // mudar a cor do texto paragrafo inicial para uma vor aleatoria
 function mudarcortexto(){
     const cores = ["#wff5733","#33ff57","#ff33a6", "#ffc133"];
-    document.getElementById("text").style.color = cores[math.floor(math.random() * cores.length)];
+    document.getElementById("text").style.color = cores[Math.floor(Math.random() * cores.length)];
 }
